46ac29f3abecc23c9c85bf6698cf6f666d8548ba
Zara Fatima Siddiqui <154442732+ZaraFatima123@users.noreply.github.com>
2025-02-08 09:15:16 +0530

    Naa.jsx

--- src/components/Naa.jsx
+++ src/components/Naa.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { FaUserCircle } from "react-icons/fa";
+import "./Navbar.css";
 
 const Naa = () => {
   const [darkMode, setDarkMode] = useState(false);
